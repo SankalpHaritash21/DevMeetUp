04ed9931a2465917788fc8c44eb57461f3b2ff18
Sankalp <sanklpkaushik21@gmail.com>
2024-11-10 19:43:40 +0530

    Database added

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
 console.log("Dev Tinder Backend File");
 
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const AdminAuth = require("./middleware/middleware");
-require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: "https://example.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
-
+const ConnectDB = require("./config/database");
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  // try {
-  //   res.send("Welcome to Dev Tinder Backend");
-  // } catch (error) {
-  //   console.error(error);
-  // }
-
-  throw new Error("Something went wrong");
-  res.send("Welcome to Dev Tinder Backend");
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) res.status(500).send("Something went wrong");
-});
-
-app.listen(PORT, () => {
-  console.log(`App is running on PORT ${PORT}`);
-});
+ConnectDB()
+  .then(() => {
+    console.log("Database Connection Successfully");
+    app.listen(PORT, () => {
+      console.log(`App is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error in Connecting Database", err);
+  });
--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(`${process.env.MONGODB_URL}`)
-  .then(() => {
-    console.log("Database Connected Successfully");
-  })
-  .catch((err) => {
-    console.log("Error in Connecting Database", err);
-  });
+const ConnectDB = async () => {
+  await mongoose.connect(`${process.env.MONGODB_URL}`);
+};
+
+module.exports = ConnectDB;
